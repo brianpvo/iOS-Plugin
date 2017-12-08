2d93d4bb8b948f291794917beba81300892e1a46
NavisensJoseph <joseph.chen@navisens.com>
2017-12-08 15:00:27 -0800

    Disconnect from server if error

---
+++ navishare/0.2.1/NaviShare.framework/Headers/NaviShare.h
@@ -0,0 +1,19 @@
+//
+//  NaviShare.h
+//  NaviShare
+//
+//  Created by nav on 12/7/17.
+//  Copyright © 2017 Navisens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NaviShare.
+FOUNDATION_EXPORT double NaviShareVersionNumber;
+
+//! Project version string for NaviShare.
+FOUNDATION_EXPORT const unsigned char NaviShareVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NaviShare/PublicHeader.h>
+
+
