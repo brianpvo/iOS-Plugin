8edd72fd6c23c9c652f7352ee4257f19279913bf
NavisensJoseph <joseph.chen@navisens.com>
2018-02-07 17:53:56 -0800

    added ALL to navisenscore

---
+++ navisenscore/0.1.0/NavisensCore.framework/Headers/NavisensCore.h
@@ -0,0 +1,19 @@
+//
+//  NavisensCore.h
+//  NavisensCore
+//
+//  Created by Joseph Chen on 10/6/17.
+//  Copyright © 2017 Navisens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NavisensCore.
+FOUNDATION_EXPORT double NavisensCoreVersionNumber;
+
+//! Project version string for NavisensCore.
+FOUNDATION_EXPORT const unsigned char NavisensCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NavisensCore/PublicHeader.h>
+
+
